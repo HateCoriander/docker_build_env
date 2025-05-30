490f3f5e775dd870d63d23126185033ded0f93f0
pmonsterR <602664701@qq.com>
2025-05-30 15:04:58 +0800

    [Initial project]
    [Description]
        - Add script downloads and enter the docker container.
        - Root password is 'luckfox', you can enter 'su root' to use super user.
        - Use './enter_docker_env.sh' to enter container.
    
    Signed-off-by: Rui Chung <602664701@qq.com>

---
+++ enter_docker_env.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+HOSTNAME=luckfoxPico
+TIMEOUT=300	# %d seconds
+IMAGES="ghcr.io/hatecoriander/luckfox_pico:latest"
+PASSWD="/etc/passwd"
+GROUP="/etc/group"
+
+DOCKERID=$(sed -n 's/^docker:[^:]*:\([^:]*\):.*$/\1/p' /etc/group)
+TOPDIR=$(dirname $(realpath $0))
+SHADOW="/etc/shadow"
+SHADOWTMP="$TOPDIR/shadow.in"
+SHADOWOVERLAY="$TOPDIR/shadow.overlay"
+
+STATUS=1
+
+if ! which docker > /dev/null 2>&1; then
+    echo "Error: Please try again after installed docker!"
+    exit $STATUS
+fi
+
+echo "Info: Update docker images from $IMAGES ..."
+timeout $TIMEOUT docker pull $IMAGES
+STATUS=$?
+if [ $STATUS -eq 124 ]; then
+    echo "Error: Please check network, docker pull timeout!"
+    exit $STATUS
+elif [ $STATUS -ne 0 ]; then
+    echo "Error: Please make sure images exist!"
+    exit $STATUS
+fi
+
+echo "Info: Generate the shadow of the overlay"
+sed "s/<user>/$(id -un)/g" $SHADOWTMP > $SHADOWOVERLAY
+
+echo -e "Info: Enter container for uid:$(id -u) gid:$(id -g) user:$(id -un)\n"
+docker run -ti --rm --user $(id -u):$(id -g) --group-add $DOCKERID \
+	--hostname $HOSTNAME --workdir $HOME \
+	-v $HOME:$HOME \
+	-v $PASSWD:$PASSWD:ro \
+	-v $GROUP:$GROUP:ro \
+	-v $SHADOWOVERLAY:$SHADOW:ro \
+	$IMAGES /bin/bash
+
+STATUS=$?
+exit $STATUS
